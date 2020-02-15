b08d1a905b7c9353f22c4fe8efa2583220ebb996
meseeks <nizam.alesevic@gmail.com>
2020-02-15 20:27:50 +0100

    all

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -1,10 +1,10 @@
 <?php
 class Config{
   const DB = [
-    'host' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'scheme' => 'taxi'
+    "host" => "sql113.epizy.com",
+    "username" => "epiz_25208193",
+    "password" => "infinityfree123",
+    "scheme" => "epiz_25208193_heytaxi"
   ];
   const JWT_SECRET = "taxitaxi";
 }
